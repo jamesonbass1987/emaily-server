a6f2ca130f8dc9f6f47381a65303b2c50350c4d4
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-03 09:44:19 -0700

    added google oauth id/secret object

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 
 const app = express();
 
 //create new instance of passport google strategy
-passport.use(new GoogleStrategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: ""
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: ""
     },
-    function(accessToken, refreshToken, profile, cb){
-        User.findOrCreate({ googleId: profile.id}, function (err, user) {
-            return cb(err, user);
-        })
+    function(accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function(err, user) {
+        return cb(err, user);
+      });
     }
-));
-
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
