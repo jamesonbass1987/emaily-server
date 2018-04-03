5128734896bf91e849d65f5c46de8e6fcfd4d9d7
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-03 09:29:53 -0700

    added basic passport.js setup

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there new'})
-});
+//create new instance of passport google strategy
+passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: ""
+    },
+    function(accessToken, refreshToken, profile, cb){
+        User.findOrCreate({ googleId: profile.id}, function (err, user) {
+            return cb(err, user);
+        })
+    }
+));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
