e5ace4eda7009aaf1e3dfb853e8d3a3305fd62bf
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-18 21:09:08 -0700

    added login and logout auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,14 @@ module.exports = app => {
 
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    app.get('/api/logout', (req, res) => {
+        //logout function automatically attached to passport
+        //takes the id cookie and removes it
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
 };
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,21 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys")
+const mongoose = require('mongoose');
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  //done is a callback to call after work is done
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +25,28 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken: ", accessToken);
-      console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
+
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser){
+            // we already have a record with profile id
+            //call done. first argument is an error arg
+            //second arg is the found resource
+            done(null, existingUser);
+          } else {
+            // create a new user, don't have a current user
+            // create new user async, then save, then return
+            //user with done function arg
+
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => {
+                //call done. first argument is an error arg
+                //second arg is the found resource
+                done(null, user);
+              });
+          }
+        });      
     }
   )
 );
